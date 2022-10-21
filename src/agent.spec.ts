7f841869ee8f0a7f456d54a792feb7f53e8e0174
esdras-santos <esantoz@protonmail.com>
2022-10-21 15:32:41 -0300

    finishing

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -1,39 +1,66 @@
-import {
-  FindingType,
-  FindingSeverity,
-  Finding,
-  HandleTransaction,
-  createTransactionEvent,
-  ethers,
-} from "forta-agent";
-import agent, {
-} from "./agent";
+import { FindingType, FindingSeverity, Finding, HandleTransaction } from "forta-agent";
+import { Interface } from "ethers/lib/utils";
+import { provideHandleTransaction } from "./agent";
+import { DAI_ADDR, TRANSFER_EVENT } from "./utils";
+import { TestTransactionEvent } from "forta-agent-tools/lib/test";
+import { createAddress } from "forta-agent-tools";
 
 describe("Invariant violation monitor", () => {
   let handleTransaction: HandleTransaction;
-  const mockTxEvent = createTransactionEvent({} as any);
+  let events: Interface
+  let mockPool: string = createAddress("0x034")
+  
+  // do mock event
 
   beforeAll(() => {
-    handleTransaction = agent.handleTransaction;
+    events = new Interface([TRANSFER_EVENT])
+    handleTransaction = provideHandleTransaction(TRANSFER_EVENT);
   });
 
   it("should return 0 findings if transfer event is not emitted", async ()=> {
+    let findings: Finding[];
+    let txEvent: TestTransactionEvent;
+
+    txEvent = new TestTransactionEvent();
+    findings = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  })
+
+  it("should return 0 findings when trensfer event is not emitted for none of the escrows", async ()=> {
+    let findings: Finding[];
+    let txEvent: TestTransactionEvent;
+
+    txEvent = new TestTransactionEvent().addEventLog(events.getEvent("Transfer"), DAI_ADDR, [
+      createAddress("0x10"),
+      10,
+      createAddress("0x11"),
+    ]);
+    findings = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  })
+
+  it("should return 0 findings when transfer event is not emitted by the DAI contract", async ()=> {
+    let findings: Finding[];
+    let txEvent: TestTransactionEvent;
+
+    txEvent = new TestTransactionEvent().addEventLog(events.getEvent("Transfer"), mockPool, [
+      createAddress("0x10"),
+      10,
+      createAddress("0x11"),
+    ]);
+    findings = await handleTransaction(txEvent);
+
+    expect(findings).toStrictEqual([]);
+  })
+
+  it("should return 1 finding when event is emitted by DAI and is for one of the escrows", async ()=> {
+
+  })
+
+  it("should return 2 findings when event is emitted by DAI and is for both escrows", async ()=> {
     
   })
 
-  it("should return 0 findings when trenfer event is not emitted for none of the escrows", async ()=> {
-    
-  })
-
-  it("", async ()=> {
-    
-  })
-
-  it("", async ()=> {
-    
-  })
-
-  it("", async ()=> {
-    
-  })
 });
